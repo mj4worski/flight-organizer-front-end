1b4e0754ba7e9f95284e7eb30dd74ed16f5339dd
Mateusz <jaworskimateusz2@gmail.com>
2017-07-20 20:53:57 +0200

    1. Fix css style in NavMenu. - link tag fulfill list(li) element - text on logo has proper position

--- src/app/Header.js
+++ src/app/Header.js
@@ -1,22 +1,25 @@
-/* eslint-disable jsx-a11y/alt-text */
 // @flow
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.scss';
 
+const NavBar = () => (
+  <ul className="nav-bar">
+    <li className="nav-bar__item">
+      <Link to="/" className="nav-bar__item-visual">Strona glowna</Link>
+    </li>
+    <li className="nav-bar__item">
+      <Link to="/findFlight" className="nav-bar__item-visual">Znajdz lot</Link>
+    </li>
+    <li className="nav-bar__item">
+      <Link to="/login" className="nav-bar__item-visual">Logowanie</Link>
+    </li>
+  </ul>
+);
+
 export default () => (
   <div className="header">
-    <ul className="nav-bar">
-      <li className="nav-bar__item">
-        <Link to="/" className="nav-bar__item-visual">Strona glowna</Link>
-      </li>
-      <li className="nav-bar__item">
-        <Link to="/findFlight" className="nav-bar__item-visual">Znajdz lot</Link>
-      </li>
-      <li className="nav-bar__item">
-        <Link to="/login" className="nav-bar__item-visual">Logowanie</Link>
-      </li>
-    </ul>
+    <NavBar />
     <div className="header__logo">
       <h1 className="header__text">Kazda podroz ma swoj poczatek</h1>
     </div>
